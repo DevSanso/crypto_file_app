de16d7e7d74dc4f87461e2063738cf06a68aca93
SansoNoia <devsanso7110@gmail.com>
2021-01-21 22:34:01 +0900

    loading widget

---
+++ lib/loading.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animations/loading_animations.dart';
+
+
+class Loading extends StatelessWidget {
+
+  Stream<String> messageQueue;
+  Loading.setStream(this.messageQueue);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Row(
+          children: 
+          [
+            loadingAni(),
+            textBox()
+          ],
+        ),
+      )
+    );
+  }
+  Widget loadingAni() {
+    return Container(
+      margin: EdgeInsets.only(top : 10),
+      child: Center(
+        child:  LoadingRotating.square(
+          borderColor: Colors.blueGrey,
+          borderSize: 3,
+          size : 30,
+          duration: Duration(milliseconds: 500),
+        ),
+      ),
+    );
+  }
+
+
+  Widget textBox() {
+    return StreamBuilder(builder: 
+      (context,AsyncSnapshot<String> snapshot) => Text(
+        snapshot.data
+      )
+    );
+  }
+}
